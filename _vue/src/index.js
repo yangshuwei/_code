dfb5acb42820780d796b6274524469c586619575
yangshuwei <yangshuwei@haier.com>
2020-10-28 17:54:06 +0800

    up

--- _vue/src/index.js
+++ _vue/src/index.js
@@ -12,4 +12,39 @@ lifecycleMixin(Vue)
 renderMixin(Vue)
 stateMixin(Vue)
 initGlobalApi(Vue)
+
+
+// 初始化方法
+
+//为了看到diff的整个流程 创建两个虚拟节点来进行比对操作
+import {compileToFunctions} from './compile/index';
+import {createElm,patch} from './vdom/patch'
+let vm1 = new Vue({data:{name:'zf'}});
+let render1 = compileToFunctions(`<div>
+   <li style="background:red" key="A">A</li>
+   <li style="background:yellow" key="B">B</li>
+   <li style="background:pink" key="C">C</li>
+   <li style="background:green" key="D">D</li>
+</div>`);
+let vnode1 = render1.call(vm1); // render方法返回的是虚拟dom
+
+document.body.appendChild(createElm(vnode1))
+
+let vm2 = new Vue({data:{name:'jw'}});
+let render2 = compileToFunctions(`<div>
+   <li style="background:green" key="D">D</li>
+   <li style="background:red" key="A">A</li>
+   <li style="background:yellow" key="B">B</li>
+   <li style="background:pink" key="C">C</li>
+   
+   
+</div>`);
+let vnode2 = render2.call(vm2); // render方法返回的是虚拟dom
+
+// 用新的虚拟节点对比老的虚拟节点，找到差异 去更新老的dom元素
+
+
+setTimeout(() => {
+   patch(vnode1,vnode2); // 传入新的虚拟节点和老的 做一个对比
+}, 3000);
 export default Vue;
--- _vue/src/vdom/patch.js
+++ _vue/src/vdom/patch.js
@@ -1,39 +1,181 @@
-export function patch(oldVnode,vnode){
-  let el = createElm(vnode) //生成真是dom
-  let parentElm = oldVnode.parentNode; //#app的父亲 =》body
-  parentElm.insertBefore(el, oldVnode.nextSibling); 
-  parentElm.removeChild(oldVnode)
-  return el
-}
+export function patch(oldVnode, vnode) {
+  if (!oldVnode) {
+    return createElm(vnode)
+  }
+  // 默认初始化时 是直接用虚拟节点创建出真实节点来 替换掉老节点
+  if (oldVnode.nodeType == 1) { //说明是真实节点
+    let el = createElm(vnode) //生成真是dom
+    let parentElm = oldVnode.parentNode; //#app的父亲 =》body
+    parentElm.insertBefore(el, oldVnode.nextSibling);
+    parentElm.removeChild(oldVnode)
+    return el
+  } else {
 
-function createElm(vnode){ 
-  let {tag,children,data,key,text} = vnode;
-  if(typeof tag == 'string'){
-    vnode. el =document.createElement(tag);
-    updateProperties(vnode)
-    children.forEach(child=>{
+    // 在更新的时 拿老的虚拟节点 和 新的虚拟节点做对比 ，将不同的地方更新真实的dom
+    // 更新功能
+    // 那当前节点 整个
+    // 1.比较两个元素的标签 ，标签不一样直接替换掉即可
+    if (oldVnode.tag !== vnode.tag) {
+      return oldVnode.el.parentNode.replaceChild(createElm(vnode), oldVnode.el)
+    }
+
+    // 2.有种可能是标签一样 <div>1</div>   <div>2</div>
+    //  文本节点的虚拟节点tag 都是undefined
+    if (!oldVnode.tag) {
+      if (oldVnode.text != vnode.text) {
+        return oldVnode.el.textContent = vnode.text
+      }
+    }
+    // 到这就说明 老的标签和新的标签一样直接复用即可
+    let el = vnode.el = oldVnode.el; // 复用老节点
+    //更新属性，用新的虚拟节点的属性和老的比较，去更新节点
+    // 新老属性做对比
+    updateProperties(vnode, oldVnode.data);
+
+
+    //比较儿子
+    let oldChildren = oldVnode.children || [];
+    let newChildren = vnode.children || [];
+    if (oldChildren.length > 0 && newChildren.length > 0) { //老的有儿子，新的也有儿子
+      updateChildren(oldChildren, newChildren, el)
+    } else
+      // 1、老的有儿子新的没有，直接把老的儿子删除掉
+      if (oldChildren.length > 0 && newChildren.length == 0) {
+        el.innerHTML = ''
+      } else if (oldChildren.length == 0 && newChildren.length > 0) { //新的有，老的没有
+        for (let i = 0; i < newChildren.length; i++) {
+          let child = newChildren[i];
+          el.appendChild(createElm(child))
+        }
+      }
+
+  }
+
+}
+function isSameVnode(oldVnode, newVnode) {
+  return (oldVnode.tag == newVnode.tag) && (oldVnode.key == newVnode.key)
+}
+function updateChildren(oldChildren, newChildren, parent) {
+  let oldStartIndex = 0;//老的索引，开头指针
+  let oldStartVnode = oldChildren[0] //老的索引指向的节点
+  let oldEndIndex = oldChildren.length - 1
+  let oldEndVnode = oldChildren[oldEndIndex]
+
+
+  let newStartIndex = 0;//新的索引，开头指针
+  let newStartVnode = newChildren[0] //新的索引指向的节点
+  let newEndIndex = newChildren.length - 1
+  let newEndVnode = newChildren[newEndIndex]
+  //vue中的diff算法做了很多优化
+  //dom操作有很多常见的逻辑  吧节点插入到当前儿子的头部 尾部  或者儿子 倒叙 正序
+
+  //vue2中采用的是双指针的方式
+  function makeIndexByKey(children){
+    let map = {};
+    children.forEach((item,index)=>{
+      if(item.key){
+        map[item.key] = index;
+      }
+    })
+    return map;
+  }
+
+  let map = makeIndexByKey(oldChildren);
+  //1.在尾部添加
+  // 循环 老的和新的  那个先结束 循环就停止  将多余的删除或者添加 （老的多新的少就删除）
+  while (oldStartIndex <= oldEndIndex && newStartIndex <= newEndIndex) { //比价谁先循环完毕  停止
+    
+    if (isSameVnode(oldStartVnode, newStartVnode)) { //如果俩人开始标签一样  就继续比较儿子 从头开始对比
+      patch(oldStartVnode, newStartVnode) //更新属性 并且递归更新额儿子
+      oldStartVnode = oldChildren[++oldStartIndex]  //指针后移比较下一个元素
+      newStartVnode = newChildren[++newStartIndex]  //指针后移比较下一个元素
+    } else if (isSameVnode(oldEndVnode, newEndVnode)) { //如果俩人开始标签不一样结束标签 就继续比较儿子  从尾开始对比
+      patch(oldEndVnode, newEndVnode);
+      oldEndVnode = oldChildren[--oldEndIndex]
+      newEndVnode = newChildren[--newEndIndex]
+    } else if (isSameVnode(oldEndVnode, newStartVnode)) { //老的尾和新的头对比  老：A B C D  新：D A B C
+      patch(oldEndVnode, newStartVnode);
+      parent.insertBefore(oldEndVnode.el, oldStartVnode.el);
+      oldEndVnode = oldChildren[--oldEndIndex];
+      newStartVnode = newChildren[++newEndIndex];
+    } else if (isSameVnode(oldStartVnode, newEndVnode)) { //老的头和新的尾对比  老：A B C D  新：D C B A
+      patch(oldStartVnode, newEndVnode);
+      //将当前元素插入到尾部的下一个元素的前面
+      parent.insertBefore(oldStartVnode.el, oldEndVnode.el.nextSibling);
+     
+      oldStartVnode = oldChildren[++oldStartIndex];
+      newEndVnode = newChildren[--newEndIndex];
+    }else{ //儿子没关系 暴力比对
+
+    }
+  }
+  if (newStartIndex <= newEndIndex) {
+    //while循环后 新的vdom儿子比老的多，就把多的插入到父节点中去
+    // 将新的多余的插入进去即可 ,可能是向前添加 还有可能是向后添加
+    // parent.appendChild(createElm(newChildren[i]));
+    // 向后插入 ele = null
+    // 像前插入 ele 就是当前像谁前面插入
+    for (let i = newStartIndex; i <= newEndIndex; i++) {
+      let ele = newChildren[newEndIndex + 1] == null ? null : newChildren[newEndIndex + 1].el;
+      parent.insertBefore(createElm(newChildren[i]), ele)
+    }
+  }
+  // 老的节点还有没处理的，说明这些老节点是不需要的节点，如过这里面有null说明，这个节点已经被处理过了，跳过即可
+  if (oldStartIndex <= oldEndIndex) {
+    for (let i = oldStartIndex; i <= oldEndIndex; i++) {
+      let child = oldChildren[i];
+      if (child != null) {
+        parent.removeChild(child.el);
+      }
+    }
+  }
+
+}
+export function createElm(vnode) {
+  let { tag, children, data, key, text } = vnode;
+  if (typeof tag == 'string') {
+    vnode.el = document.createElement(tag); //创建元素 放到vnode.el上
+    updateProperties(vnode) //更新属性
+    children.forEach(child => { //遍历儿子，将儿子渲染后的结果放到父亲种
       vnode.el.appendChild(createElm(child))
     })
-  }else{
+  } else {
     vnode.el = document.createTextNode(text)
   }
   // console.log(vnode.el)
   return vnode.el
 }
-
-function updateProperties(vnode){
+// vue 的渲染流程 =》 先初始化数据 =》 将模板进行编译 =》 render函数 =》 生成虚拟节点 =》 生成真实的dom  =》 扔到页面上
+function updateProperties(vnode, oldProps = {}) {
   let newProps = vnode.data || {}
   let el = vnode.el;
-  for(let key in newProps){
-    if(key == 'style'){
-      for(let styleName in newProps.style){
+  //老的有属性，新的没有  直接删除老的
+  for (let key in oldProps) {
+    if (!newProps[key]) {
+      el.removeAttribute(key)
+    }
+  }
+
+  //处理样式  老的 color:red  新的 background：red
+  let newStyle = newProps.style || {};
+  let oldStyle = oldProps.style || {}
+  for (let key in oldStyle) {
+    // 老的样式中有 新的没有 删除老的样式
+    if (!newStyle[key]) {
+      el.style[key] = ""
+    }
+  }
+  //新的有直接更新老的就可以
+  for (let key in newProps) {
+    if (key == 'style') {
+      for (let styleName in newProps.style) {
         el.style[styleName] = newProps.style[styleName]
       }
-    }else if (key === 'class'){
+    } else if (key === 'class') {
       el.className = newProps.class
-    }else{
+    } else {
       el.setAttribute(key, newProps[key])
     }
-    
+
   }
 }
