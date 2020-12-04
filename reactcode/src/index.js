ab53c105c97d4d27cc1ab8f7f114e64d59485206
yangshuwei <yangshuwei@haier.com>
2020-12-04 18:04:18 +0800

    up

--- reactcode/src/index.js
+++ reactcode/src/index.js
@@ -1,14 +1,17 @@
 import React from './react';
 import ReactDOM from './react-dom';
 
-let element  = React.createElement('h1',{
-  className:'title',
-  style:{
-    color:"red"
-  }
-},"hello")
-console.log(element)
+// let element  = React.createElement('h1',{
+//   className:'title',
+//   style:{
+//     color:"red"
+//   }
+// },"hello")
+
+function Welcome(props){
+  return <h1>hello,<span>{props.name}</span></h1>
+}
 ReactDOM.render(
-  element,
+  <Welcome name="word"/>,
   document.getElementById('root')
 );
--- reactcode/src/react-dom.js
+++ reactcode/src/react-dom.js
@@ -1,21 +1,59 @@
 function render(vdom,container){
     // console.log(vdom,container)
     let dom = createDOM(vdom);
-    console.log(dom)
     container.appendChild(dom);
 }
 
 function createDOM(vdom){
-    let {type,props} = vdom
-    let dom;
-    if(typeof type === 'string' || typeof type === 'number'){
-        return document.createTextNode(type);
+    if (typeof vdom === 'string' || typeof vdom === 'number') {
+        return document.createTextNode(vdom);
     }
-    dom = document.createElement(type)
+    if(!vdom){
+        return '';
+    }
+   
+    let {type,props} = vdom
+    let dom
+    // if (typeof vdom === 'string' || typeof vdom === 'number') {
+    //     return document.createTextNode(vdom);
+    // } else 
+    if (typeof type == 'function') {
+        console.log('函数式组件')
+        return mountFunctionComponent(vdom)
+    }else{
+        dom = document.createElement(type)
+    }
+    updateProps(dom, props)
+    if(typeof props.children === 'string' || typeof props.children === 'number'){
+        dom.textContent = props.children;
+    }else if(typeof props.children === 'object' && props.children.type){
+        render(props.children,dom)
+    }else if(Array.isArray(props.children)){
+        reconcileChildren(props.children,dom)
+    }
     return dom;
 }
-
-
-
+function reconcileChildren(childVdom,parentDom){
+    childVdom.forEach(child => render(child,parentDom));
+}
+function updateProps(dom,props){
+    for(let key in props){
+        if(key === 'children') continue;
+       if(key === 'style'){
+           let objStyle = props[key];
+           for(let key in objStyle){
+                dom.style[key] = objStyle[key]
+           }
+       }else{
+        dom[key] = props[key]
+       }
+    }
+}
+function mountFunctionComponent(vdom){
+    let {type,props} = vdom;
+    let renderVdom = type(props)
+    console.log(renderVdom)  //将函数组件解析成{type:"h1",props:{children:[]}}
+    return createDOM(renderVdom)
+}
 let ReactDOM = {render}
 export default ReactDOM;
