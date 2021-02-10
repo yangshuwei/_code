3b37f4aa3e35ce6aa19470a9a6e762e450128db3
yangshuwei <yangshuwei@haier.com>
2021-02-10 17:16:00 +0800

    up

--- react_test/src/pages/home/index.js
+++ react_test/src/pages/home/index.js
@@ -1,42 +1,16 @@
 import React, { useEffect, useState } from 'react';
-import City from "./city"
-const Home = (props) =>{
-    const [arr,setArr] = useState(0)
-    const [data,setData] = useState([
-    
-        {
-            pid:1,
-            cid:2,
-            rid:3
-        },
-        {
-            pid:2,
-            cid:4,
-            rid:6
-        }
-    ])
-    useEffect(()=>{
-        function aa(){
+class Home extends React.Component{
+    state = { number: 0 }
+    componentDidMount(){
         
-            bb()
-            console.log(arr)
-        }   
-
-        function bb(){
-            setArr(1)
-            console.log(arr)
-        }
-        aa()
-        
-    })
-     
-   
-    return(
-        <>
-        
-            
-        <City info="aa"></City>
-        </>
-    )
+        this.setState({
+            number:2
+        })
+    }
+    render(){
+        return(
+            <div>{this.state.number}</div>
+        )
+    }
 }
 export default Home
--- upload/server/1.js
+++
@@ -1,9 +0,0 @@
-var name = "spring";
-(function () {
-  console.log(name)
-  if (typeof name === 'undefined') {
-    console.log('if', name)
-  } else {
-    console.log(name)
-  }
-})()
