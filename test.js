622c463879c4b742bf626327aaf96723b79da439
yangshuwei <13120024549@163.com>
2021-03-21 22:11:27 +0800

    up

--- test.js
+++ test.js
@@ -1,30 +1,50 @@
-const arr = [1, 1, '1', 17, true, true, false, false, 'true', 'a', {}, {}];
-// => [1, '1', 17, true, false, 'true', 'a', {}, {}]
-// let obj = {};
-// let arr2 =[]
-// let typeArr =[]
-// for(let i=0;i<arr.length;i++){
-//     let item = arr[i]
-//     typeArr.push(typeof item)
-//     // console.log(typeArr)
-    
-//     if(obj[item]==null && (typeof item === typeof obj[item])){
-//         arr2.push(item)
-//         obj[item] = true;
-
-//     }
-// }
-// console.log(arr2)
-
-// function un(arr){
-//     return Array.from(new Set(arr))
-// }
-// const res = un(arr)
-// console.log(res)
-// console.log(Array.from(new Set(arr)))
 
 
-let newarr = arr.reduce(function(pre,cur){
-    return pre.includes(cur)  ? pre :pre.concat(cur)
-},[])
-console.log(newarr)
+const obj = {
+    name: 123,
+    age:null,
+    child: {
+        name: 'child'
+    }
+};
+
+function deepClone(obj) {
+    const data = {}
+    const arr = [{
+        parent: data,
+        key: null,
+        data: obj
+    }];
+
+    while (arr.length) {
+        const node = arr.shift();
+        let parent = node.parent;
+        let data = node.data;
+        let key = node.key;
+
+        let result = parent;
+        if (key) {
+            result = parent[key] = {};
+        }
+
+        for (let i in data) {
+            if (typeof data[i] && typeof data[i] === 'object') {
+                arr.push({
+                    key: i,
+                    parent: result,
+                    data: data[i]
+                })
+            } else {
+                result[i] = data[i];
+            }
+        }
+
+    }
+
+    return data;
+
+
+
+}
+
+console.log(deepClone(obj));
